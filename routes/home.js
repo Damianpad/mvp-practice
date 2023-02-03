7f7c8dc7fe34ffb125ccfb8f92b8916469c47b2f
Damian Padilla <damipad4@gmail.com>
2023-02-03 10:09:38 -0600

    init routers

--- routes/home.js
+++ routes/home.js
@@ -1,2 +1,10 @@
 // Handles initial GET request for the homepage
 // Handles POST request for adding a new item
+
+const express = require('express')
+
+const router = express.Router()
+const homeController = require()
+
+router.get('/', homeController.getIndex)
+router.post('/new', homeController.createItem)
