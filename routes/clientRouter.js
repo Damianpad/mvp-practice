3379dade9090ab7f55586040b589e0e8cf2743f1
Damian Padilla <damipad4@gmail.com>
2023-02-10 12:31:41 -0600

    updated requests for edit

--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const clientController = require('../controllers/client')
 const editClientController = require('../controllers/edit')
 
-router.get('/', clientController.getClient) //read clients
-router.post('/addClient', clientController.createClient) //Add a new client
-router.get('/edit', editClientController.getEdit)
+router.get('/', clientController.getClient) // Read clients
+router.post('/addClient', clientController.createClient) // Add a new client
+router.get('/edit', editClientController.getEdit)// Edit Existing Client
 
 module.exports = router 
--- routes/edit.js
+++ routes/edit.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const editController = require('../controllers/edit')
 
-router.get('/:id', editController.getEdit)
+router.get('/edit', editController.getEdit)
 router.get('/remove/:id', editController.deleteItem)
 router.post('/update/:id', editController.updateItem)
 
