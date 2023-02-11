3f80d67474d0adb44e29d7f06d2b37fc4397eb06
Damian Padilla <damipad4@gmail.com>
2023-02-11 02:18:57 -0600

    update edit controller

--- controllers/edit.js
+++ controllers/edit.js
@@ -2,11 +2,12 @@ const clientList = require('../models/clients')
 
 module.exports = {
     getEdit: async (req,res) => {
-        const id = req.params.id
-        console.log(id)
+        // const id = req.params.id
+        // console.log(id)
         try {
+            console.log('client edited');
             const clients = await clientList.find()
-            res.render('edit.js', {clientList: clients})
+            res.render('edit.ejs', {clientList: clients})
         }catch (err) {
             return res.status(500).send(err)
         }
